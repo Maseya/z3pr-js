4af9c07df336bd3badcad5fa206d28beeb25e321
Rebelus Quo <rebelusquo@tutanota.com>
2020-06-09 19:34:39 +0200

    Spec maseya_blend
    While the specific color adjustments are arbitrary in a general sense and evolving, for each given commit they are a fact of the implementation and we can spec for those given specifics.
    
    Due to how luma adjustments can clamp color components, some tests can only reliably verify a subset of the hcy space. We are making the assumption the rest of the input domain is calculated correctly.

---
+++ spec/maseya_blend_spec.js
@@ -0,0 +1,177 @@
+import chai from 'chai';
+chai.should();
+
+import times from 'lodash/times';
+import random from 'lodash/random';
+
+const { abs, min } = Math;
+
+import color_f from '../src/color_f';
+
+import maseya_blend from '../src/maseya_blend';
+
+const delta = 10e-9;
+const repeats = 500;
+
+describe('Maseya blend function', () => {
+
+    context('hue', () => {
+
+        it('is changed by at least +/- 2.5% when color is chromatic', () => {
+            times(repeats, () => {
+                const x = some_color();
+                if (is_grayscale(x)) return;
+
+                // Here we pick a chroma and luma that give a minimal change
+                const y1 = color_f(0, .5, 0);
+                const y2 = color_f(1, .5, 0);
+
+                const z1 = maseya_blend(x, y1);
+                const z2 = maseya_blend(x, y2);
+
+                hue_diff(x.hue(), z1.hue()).should.be.at.least(.025 - delta, 'changing clockwise');
+                hue_diff(x.hue(), z2.hue()).should.be.at.least(.025 - delta, 'changing counterclockwise');
+            });
+        });
+
+        function hue_diff(a, b) {
+            const rotate = (x) => (x + 2 / 6) % 1;
+            return min(abs(a - b), abs(rotate(a) - rotate(b)));
+        }
+
+    });
+
+    context('saturation', () => {
+
+        it('increases by an inverse scaled proportion', () => {
+            times(repeats, () => {
+                const x = some_color();
+                const y = color_f(0, random(.5 + delta, 1), .5);
+
+                const z = maseya_blend(x, y);
+                if (probably_clamped(z)) return;
+
+                const scale = .5;
+                const lower = x.chroma();
+                const upper = lower * (1 + (1 - lower) * .5 * scale);
+                z.chroma().should.be.within(lower, upper);
+            });
+        });
+
+        it('can decrease to any degree', () => {
+            times(repeats, () => {
+                const x = some_color();
+                const y = color_f(0, random(0, .5), .5);
+
+                const z = maseya_blend(x, y);
+
+                const lower = 0;
+                const upper = x.chroma();
+                z.chroma().should.be.within(lower, upper);
+            });
+        });
+
+        it('does not decrease when blended with medium chroma', () => {
+            times(repeats, () => {
+                const x = some_color();
+                const y = color_f(0, .5, .5);
+
+                const z = maseya_blend(x, y);
+
+                const x_chroma = with_minimal_hue_shift(x).chroma();
+                z.chroma().should.equal(x_chroma);
+            });
+        });
+
+        it('decreases to grayscale when blended with zero chroma', () => {
+            times(repeats, () => {
+                const x = some_color();
+                const y = color_f(0, 0, .5);
+
+                const z = maseya_blend(x, y);
+
+                z.chroma().should.equal(0);
+            });
+        });
+
+    });
+
+    context('brightness', () => {
+
+        it('increases by an inverse proportion', () => {
+            times(repeats, () => {
+                const x = some_color();
+                // Here we pick a chroma that gives a minimal change
+                const y = color_f(0, .5, random(.5 + delta, 1));
+
+                const z = maseya_blend(x, y);
+
+                const lower = with_minimal_hue_shift(x).luma();
+                const upper = lower * (1 + (1 - lower) * .5);
+                z.luma().should.be.within(lower, upper);
+            });
+        });
+
+        it('can decrease by a fraction', () => {
+            times(repeats, () => {
+                const x = some_color();
+                // Here we pick a chroma that gives a minimal change
+                const y = color_f(0, .5, random(0, .5));
+
+                const z = maseya_blend(x, y);
+
+                const upper = with_minimal_hue_shift(x).luma();
+                const lower = upper * .75;
+                z.luma().should.be.within(lower, upper);
+            });
+        });
+
+        it('increases more if decreasing saturation a lot', () => {
+            times(repeats, () => {
+                const x = some_color();
+                // Here we pick a chroma that gives a big decrease but not to grayscale
+                const y1 = color_f(0, 1 / 16, .75);
+                const y2 = color_f(0, 2 / 16, .75);
+
+                const z1 = maseya_blend(x, y1);
+                const z2 = maseya_blend(x, y2);
+
+                z1.luma().should.be.above(z2.luma());
+            });
+        });
+
+        it('does not change when blended with medium luma', () => {
+            times(repeats, () => {
+                const x = some_color();
+                const y = color_f(0, .5, .5);
+
+                const z = maseya_blend(x, y);
+
+                const x_luma = with_minimal_hue_shift(x).luma();
+                z.luma().should.equal(x_luma);
+            });
+        });
+
+    });
+
+    function is_grayscale(c) {
+        return c.chroma() <= 0;
+    }
+
+    function probably_clamped(c) {
+        return c.r >= 1 || c.g >= 1 || c.b >= 1;
+    }
+
+    function with_minimal_hue_shift(c) {
+        return color_f.from_hcy(c.hue() + .025, c.chroma(), c.luma());
+    }
+
+    function some_color() {
+        return color_f(unit(), unit(), unit());
+    }
+
+    function unit() {
+        return random(1, true);
+    }
+
+});
--- src/maseya_blend.js
+++ src/maseya_blend.js
@@ -1,33 +1,38 @@
 import color_f from './color_f';
 
-const { max } = Math;
+const { max, sqrt } = Math;
+
+const sqr = x => x * x;
 
 export default function (x, y) {
-    // Ensure at least a 5% change in hue.
+    // Ensure at least a 2.5% change in hue.
     const hue = (y.r * .95) + .025 + x.hue();
 
     const chroma_shift = y.g - .5;
-    let chroma = x.chroma();
+    const x_chroma = x.chroma();
+    let chroma = x_chroma;
     if (chroma_shift > 0) {
         // Put heavy limitations on oversaturating colors.
-        chroma *= 1 + ((1 - chroma) * chroma_shift * .5);
+        chroma *= 1 + ((1 - x_chroma) * chroma_shift * .5);
     }
     else {
-        // Put no limitation on desaturating colors.
-        chroma *= .5 + chroma_shift;
+        // Put no limitation on desaturating colors. However, make it more
+        // likely that only a little desaturation will occur.
+        chroma *= sqrt(1 - sqr(chroma_shift * 2));
     }
 
     const luma_shift = y.b - .5;
-    let luma = x.luma();
+    const x_luma = x.luma();
+    let luma = x_luma;
     if (luma_shift > 0) {
         // Do not heavily brighten colors. However, if we removed a lot of
         // saturation, then we can allow for some brighter colors.
-        const chroma_diff = max(chroma - x.chroma(), 0);
-        luma *= 1 + ((1 - x.luma()) * luma_shift * (1 + chroma_diff));
+        const chroma_diff = max(x_chroma - chroma, 0);
+        luma *= 1 + ((1 - x_luma) * luma_shift * (1 + chroma_diff));
     }
     else {
         // Do not let colors get too dark.
-        luma *= 1 + ((.5 + luma_shift) / 1);
+        luma *= 1 + (luma_shift / 2);
     }
 
     return color_f.from_hcy(hue, chroma, luma);
